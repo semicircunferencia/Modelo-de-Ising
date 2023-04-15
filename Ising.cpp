4a711d9a5a6ee987cea141f7b9d3ea72bc9ec44e
semicircunferencia <125926149+semicircunferencia@users.noreply.github.com>
2023-04-15 13:56:52 +0200

    l

--- Ising.cpp
+++ Ising.cpp
@@ -28,28 +28,43 @@ sumar el incremento de energía). CONDICIONES DE CONTORNO PERIÓDICAS
 #include <gsl/gsl_rng.h> // Números aleatorios. Cortesía de gsl
 #include <sys/time.h> // Tiempo
 
+// El número de espines es N^2
 #define N 5
-#define temp 1
-
 
 using namespace std;
 
 long int semillatiempo();
-void inicializarespines(int espines[][N], int caso);
-gsl_rng *tau;
+void inicializarespines(int espines[][N], int caso, gsl_rng *tau);
 
 /**************************************************** FUNCIÓN PRINCIPAL ****************************************************/
 int main(void) {
 
     // Declaro las variables
     int espines[N][N];
-    int caso; // La inicialización de los espines dependerá del usuario
-    extern gsl_rng *tau;
+    double temp;
+    int pasosMC;
+    int caso;
 
-    // Inicializo los espines
+    // Inicializo el puntero de números aleatorios
+    gsl_rng *tau;
+    tau=gsl_rng_alloc(gsl_rng_taus);
+    gsl_rng_set(tau,semillatiempo());
+
+    // Pido los parámetros de la simulación
+    cout << "Escoja una temperatura entre 0 y 5\n";
+    cin >> temp;
+
+    cout << "Escoja el número de pasos Monte Carlo\n";
+    cin >> pasosMC;
+
     cout << "Elija una inicialización. 1 = Aleatorios, 2 = Todos positivos, 3 = Todos negativos\n";
     cin >> caso;
-    inicializarespines(espines, caso);
+
+    // Inicializo los espines, dependiendo del caso que se haya escogido
+    inicializarespines(espines, caso, tau);
+
+    // Comienzo a iterar
+
 
     return 0;
 }
@@ -64,17 +79,11 @@ long int semillatiempo()
 }
 
 /*Función inicializarespines. Simplemente inicializa los espines*/
-void inicializarespines(int espines[][N], int caso) {
+void inicializarespines(int espines[][N], int caso, gsl_rng *tau) {
 
     // Si los inicializamos aleatoriamente (caso 1)
     if(caso==1) {
-
-        // Inicializo la semilla
-        long int semilla=semillatiempo();
-        extern gsl_rng *tau;
-        tau=gsl_rng_alloc(gsl_rng_taus);
-        gsl_rng_set(tau,semilla);
-
+        cout << "\n";
 
         // Asigno valores
         for(int i=0; i<N; i++) {
@@ -83,6 +92,8 @@ void inicializarespines(int espines[][N], int caso) {
                 // Esta función da 0 ó 1 con probabilidad 1/2. Tengo que cambiar los 0 a -1
                 espines[i][j]=gsl_rng_uniform_int(tau,2);
                 if(espines[i][j]==0) espines[i][j]=-1;
+
+                cout << espines[i][j] << "\n";
             }
         }
     }
@@ -102,4 +113,41 @@ void inicializarespines(int espines[][N], int caso) {
     }
 
     return;
+}
+
+/*Función iteracion. Es el núcleo del programa. Realiza los pasos que determinan si se cambia de signo algún
+espín escogido aleatoriamente*/
+void iteracion(int espines[][N], double temp, gsl_rng *tau) {
+
+    double p;
+    double numerito;
+
+    // En primer lugar, escojo un espín (i,j) al azar
+    int i=gsl_rng_uniform_int(tau, N);
+    int j=gsl_rng_uniform_int(tau, N);
+
+    // Calculo la p correspondiente
+    numerito=exp(incEnergia(espines, i, j)/temp);
+    
+    if(numerito>=1) p=1;
+    else p=numerito;
+
+    /*Calculo un número aleatorio entre 0 y 1. Si es <p, cambio el signo del espín. Utilizo el mismo double numerito
+    que ahora no sirve de nada más*/
+    numerito=gsl_rng_uniform(tau);
+    if(numerito<p) espines[i][j]=-espines[i][j];
+
+}
+
+/*Función incEnergia. Calcula el incremento de energía que supondría cambiar el espín (n,m) por su opuesto.
+Tiene en cuenta las condiciones periódicas*/
+double incEnergia(int espines[][N], int i, int j) {
+
+    // Calculo i+1, i-1, j+1, j-1
+    int imas1=((i+1)%N + N)%N;
+    int imenos1=((i-1)%N + N)%N;
+    int jmas1=((j+1)%N + N)%N;
+    int jmenos1=((j-1)%N + N)%N;
+
+    return -2*espines[i][j]*(espines[imas1][j]+espines[imenos1][j]+espines[i][jmas1]+espines[i][jmenos1]);
 }
