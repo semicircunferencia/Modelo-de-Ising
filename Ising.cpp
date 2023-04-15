146c24e35492f49eb7b880f8fc21642e06ef90b8
semicircunferencia <125926149+semicircunferencia@users.noreply.github.com>
2023-04-15 12:35:12 +0200

    l

--- Ising.cpp
+++ Ising.cpp
@@ -28,32 +28,78 @@ sumar el incremento de energía). CONDICIONES DE CONTORNO PERIÓDICAS
 #include <gsl/gsl_rng.h> // Números aleatorios. Cortesía de gsl
 #include <sys/time.h> // Tiempo
 
+#define N 5
+#define temp 1
+
+
 using namespace std;
 
 long int semillatiempo();
-
+void inicializarespines(int espines[][N], int caso);
 gsl_rng *tau;
 
+/**************************************************** FUNCIÓN PRINCIPAL ****************************************************/
 int main(void) {
+
+    // Declaro las variables
+    int espines[N][N];
+    int caso; // La inicialización de los espines dependerá del usuario
     extern gsl_rng *tau;
-    double x[10];
 
-    long int semilla=semillatiempo();
-    tau=gsl_rng_alloc(gsl_rng_taus);
-    gsl_rng_set(tau,semilla);
-
-    for(int i=0; i<10; i++) {
-        x[i]=gsl_rng_uniform(tau);
-        cout << x[i] << "\n";
-    }
+    // Inicializo los espines
+    cout << "Elija una inicialización. 1 = Aleatorios, 2 = Todos positivos, 3 = Todos negativos\n";
+    cin >> caso;
+    inicializarespines(espines, caso);
 
     return 0;
 }
+/***************************************************************************************************************************/
 
-/*FUNCIÓN SEMILLATIEMPO. Crea una semilla para generar números aleatorios, basada en el tiempo actual.*/
+/*Función semillatiempo. Crea una semilla para generar números aleatorios, basada en el tiempo actual.*/
 long int semillatiempo()
 {
     struct timeval tv;
     gettimeofday(&tv,0);
     return (tv.tv_sec + tv.tv_usec);
+}
+
+/*Función inicializarespines. Simplemente inicializa los espines*/
+void inicializarespines(int espines[][N], int caso) {
+
+    // Si los inicializamos aleatoriamente (caso 1)
+    if(caso==1) {
+
+        // Inicializo la semilla
+        long int semilla=semillatiempo();
+        extern gsl_rng *tau;
+        tau=gsl_rng_alloc(gsl_rng_taus);
+        gsl_rng_set(tau,semilla);
+
+
+        // Asigno valores
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+
+                // Esta función da 0 ó 1 con probabilidad 1/2. Tengo que cambiar los 0 a -1
+                espines[i][j]=gsl_rng_uniform_int(tau,2);
+                if(espines[i][j]==0) espines[i][j]=-1;
+            }
+        }
+    }
+
+    // Si todos positivos (caso 2)
+    else if(caso==2) {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) espines[i][j]=1;
+        }
+    }
+
+    // Si todos negativos (caso 3)
+    else {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) espines[i][j]=-1;
+        }
+    }
+
+    return;
 }
